b0dae1925ab67b2e5d96e7103882cf6e7c052e04
Priya <Priyamadiwalar0007@gmail.com>
2022-02-07 02:14:44 +0530

    added: LineComparison

--- src/com/linecoparison/LineComparisonComputation.java
+++ src/com/linecoparison/LineComparisonComputation.java
@@ -1,10 +1,23 @@
   package com.linecoparison;
-
-     public class LineComparisonComputation {
-
+  
+    import java.util.Scanner;
+    
+    
+      public class LineComparisonComputation {
+ 
 	      public static void main(String[] args) {
-		     // TODO Auto-generated method stub
-
+		     
+	    	  Scanner sc = new Scanner(System.in);
+                
+	    	        System.out.println("Enter the Co-ordinates of x1 , x2 and y1, y2 : ");
+	    	  
+	                      int x1 = sc.nextInt();
+	                      int y1 = sc.nextInt();
+	                      int x2 = sc.nextInt();
+	                      int y2 = sc.nextInt();
+	        
+	        double len= Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
+	                 System.out.println("Length of the given Line is : " + len + "units" );
 	 }
 
  }
