8e29a1412bc7c0075bf4b696542558ee6a36a8c1
Priya <Priyamadiwalar0007@gmail.com>
2022-02-07 02:38:06 +0530

    added: LineComparison

--- src/com/linecoparison/LineComparisonComputation.java
+++ src/com/linecoparison/LineComparisonComputation.java
@@ -15,9 +15,29 @@
 	                      int y1 = sc.nextInt();
 	                      int x2 = sc.nextInt();
 	                      int y2 = sc.nextInt();
+	                      
+	        double len1= Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
+	                     System.out.println("Length of the given Line is : " + len1 + "units" );
 	        
-	        double len= Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
-	                 System.out.println("Length of the given Line is : " + len + "units" );
+	                System.out.println("Enter the Co-ordinates of p1 , p2 and q1, q2 : ");
+	        	    	  
+	                      int p1 = sc.nextInt();
+	                      int q1 = sc.nextInt();
+	                      int p2 = sc.nextInt();
+	                      int q2 = sc.nextInt();  
+	                      
+	        double len2= Math.sqrt((p2 - p1) * (p2 - p1)  +  (q2 - q1) * (q2 - q1));
+	                      System.out.println("Length of the given Line is : " + len2 + "units" );
+	                 
+	                 if (len1 == len2) 
+	             	{
+	             		System.out.println("Both lines are equale");
+	             	}
+	             	else
+	             	{
+	             		System.out.println("Both lines are not equale");	
+	             	}
+	             		
 	 }
 
  }
